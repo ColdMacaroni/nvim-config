029fe0c630e75176d16ebb2e86cc6f1736c712fa
Sofa <squishysparrow7.7@gmail.com>
2023-09-25 21:32:33 +1300

    Processing: Create tags file on save
    The lsp doesn't do defintions so this helps

--- config/ftplugin/processing.lua
+++ config/ftplugin/processing.lua
@@ -1,10 +1,18 @@
 -- Let's go gamers
 
+local root_dir = vim.fs.dirname(vim.fs.find(function(name)
+  return name:match ".*%.pde$"
+end, { type = "file" })[0])
+
 vim.lsp.start {
   name = "processing-lsp",
   cmd = { "/usr/share/processing/processing-lsp" },
-  root_dir = vim.fs.dirname(vim.fs.find(function(name)
-    return name:match ".*%.pde$"
-  end, { type = "file" })[0]),
+  root_dir = root_dir,
 }
 
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.pde" },
+  callback = function()
+    vim.cmd [[silent! !ctags --langmap=java:+.pde -R]]
+  end,
+})
