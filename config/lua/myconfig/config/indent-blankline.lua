7d16a7254e746f64afdb6241e9e606106406e00c
Sofa <squishysparrow7.7@gmail.com>
2023-11-01 21:35:16 +1300

    Messs with indent blankline
    not too sure of what i did /shrug

--- config/lua/myconfig/config/indent-blankline.lua
+++ config/lua/myconfig/config/indent-blankline.lua
@@ -1,19 +1,28 @@
 local ibl = require "ibl"
 ibl.setup()
 
+local config = require "ibl.config".config
+
+-- config.scope.char = ";"
+
 local hooks = require "ibl.hooks"
 hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
 
+-- local highlights = {"IndentBlanklineChar"}
+-- hooks.register(hooks.type.HIGHLIGHT_SETUP, function() 
+--
+-- end)
+--
 -- {{{ AutoCMD: Make background lines darker
 -- It will look kinda ugly in light themes but like that's your problem  ( ◕ ◡ ◕ ✿ )
-vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-  callback = function()
-    -- Can't do this from the nvim API
-    if vim.g.transparent_enabled then
-      vim.cmd "highlight IndentBlanklineChar guifg=#202020 gui=nocombine"
-    else
-      vim.cmd "highlight IndentBlanklineChar guifg=#303040 gui=nocombine"
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+--   callback = function()
+--     -- Can't do this from the nvim API
+--     if vim.g.transparent_enabled then
+--       vim.cmd "highlight IndentBlanklineChar guifg=#202020 gui=nocombine"
+--     else
+--       vim.cmd "highlight IndentBlanklineChar guifg=#303040 gui=nocombine"
+--     end
+--   end,
+-- })
 -- }}}
